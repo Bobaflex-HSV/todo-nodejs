ec0cf904e8eff540de10ae81d79d9c60624ae0a6
Jens Duray <bobaflex.hsv@gmail.com>
2020-05-04 14:01:21 +0200

    add server and server listen

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+
+// use port set by environment variable if available, else set port to 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}...`)
+);
